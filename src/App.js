bfa92c8da2a52413d740f462b89ab86e51dd9caf
Luay Alali <luayalali@gmail.com>
2020-11-11 16:54:41 -0800

    used the Fetch method to fetch data from the given api.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ export default class App extends Component {
   
   constructor() {
     super();
+    this.state = {
+      gifs: []
+    };
   } 
 
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    .then( response => response.json() )
+    .then(responseData => {
+      this.setState(
+        {gifs: responseData.data}
+      );
+    })
+    .catch(error =>  {
+      console.log('Error fetching and parsing Data', error);
+    });
+  }
+
   render() { 
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
